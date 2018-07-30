912d0cf79d480babb6b13618223fb499fc97542c
tripp528 <tripp528@gmail.com>
2018-07-30 09:50:19 -0600

    Installed bourbon, cleaned up footer

--- php/header.php
+++ php/header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <header>
-    <h1 id="hello">Hello Wordd</h1>
+
+  <div id="menu">
     <a href="index.php?page=home" class="button">home</a>
     <a href="index.php?page=content" class="button">content</a>
+    <a href="index.php?page=content" class="button">about</a>
+    <a href="index.php?page=content" class="button">my stuff</a>
+    <a href="index.php?page=content" class="button">something</a>
+    <a href="index.php?page=content" class="button">random</a>
+  </div>
+    <h1 id="hello">Hello Wordd</h1>
 </header>
